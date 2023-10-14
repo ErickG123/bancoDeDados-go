3fd5b65c538fbe148d7678749c7b9f67a86ba894
Erick Gabriel <erickgabrielalves@hotmail.com>
2023-10-14 20:53:24 -0300

    belongs_to e has_one

---
+++ 3/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql" // Driver do MySQL
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID           int `gorm:"primaryKey"`
+	Name         string
+	Price        float64
+	CategoryID   int
+	Category     Category
+	SerialNumber SerialNumber
+	gorm.Model
+}
+
+type Category struct {
+	ID   int `gorm:"primaryKey"`
+	Name string
+	gorm.Model
+}
+
+type SerialNumber struct {
+	ID        int `gorm:"primaryKey"`
+	Number    string
+	ProductID int
+	gorm.Model
+}
+
+func main() {
+	// Conexão com o Banco de Dados
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&Product{}, &Category{}, &SerialNumber{})
+
+	// Trabalhando com Relacionamentos
+	// belongs_to => Cada Produto pertence a uma Categoria específica
+	category := Category{Name: "Eletronicos"}
+	db.Create(&category)
+
+	db.Create(&Product{
+		Name:       "Notebook",
+		Price:      1000.00,
+		CategoryID: category.ID,
+	})
+
+	db.Create(&SerialNumber{
+		Number:    "123456",
+		ProductID: 1,
+	})
+
+	// Selecionando os Produtos
+	var products []Product
+	// Dando um Preload na tabela de Categorias para
+	// o ORM exibir os dados
+	db.Preload("Category").Find(&products)
+	for _, product := range products {
+		fmt.Println(product.Name, product.Category.Name)
+	}
+
+	// has_one => Relacionamento 1:1
+	db.Preload("Category").Preload("SerialNumber").Find(&products)
+	for _, product := range products {
+		fmt.Println(product.Name, product.Category.Name, product.SerialNumber.Number)
+	}
+
+	// has_many => Relacionamento 1:N
+}
